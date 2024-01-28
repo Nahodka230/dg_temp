9bd27d063d5715fd4afeee7d1e19478b1a03fa03
Nahodka230 <nahodka230@gmail.com>
2024-01-28 10:39:46 +0500

    try1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,17 @@
+import datetime
 from django.db import models
-
+from django.forms import DateField, BooleanField, SlugField
+from pytils.translit import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length = 50)
+    price = models.IntegerField()
+    image = models.URLField(max_length = 200)
+    release_date = models.DateField(default=datetime.date.today)
+    lte_exists = models.BooleanField(default=False)
+    slug = models.CharField(max_length = 50, default='')
+
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self.name)
+    #         return super().save(*args, **kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_by = request.GET.get('sort', '')
+    #     phones = phones.order_by(sort_by)
+    if sort_by:
+        match sort_by:
+            case 'name':
+                phones = Phone.objects.order_by('name')
+            case 'min_price':
+                phones = Phone.objects.order_by('price')
+            case 'max_price':
+                phones = Phone.objects.order_by('-price')
+    else:
+        phones = Phone.objects.all()
+    context = {"phones": phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = Phone.objects.get(slug=slug)
     template = 'product.html'
-    context = {}
+    context = {"phone": phone}
     return render(request, template, context)
